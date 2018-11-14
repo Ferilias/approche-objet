16a00730d6f6579bb2627f1adde4f29e7dce9159
alexandre <alex.ayato@gmail.com>
2018-11-14 15:55:22 +0100

    Good commit

--- src/entites/Personne.java
+++ src/entites/Personne.java
@@ -1,5 +1,12 @@
 package entites;
 
 public class Personne {
+	
+	public String nom;
+	public String prenom;
+	public String adresse;
 
+public void affichage(){
+	System.out.println("nom : "+this.nom +" prenom : "+ this.prenom);
+}
 }
--- src/essais/TestPersonne.java
+++ src/essais/TestPersonne.java
@@ -1,5 +1,22 @@
 package essais;
+import entites.Personne;
 
 public class TestPersonne {
+
+	public static void main(String[] args) {
+		
+		Personne p1 = new Personne();
+		p1.nom = "Durand";
+		p1.prenom = "Jean";
+		p1.adresse = "22 rue des glands";
+		
+		Personne p2 = new Personne();
+		p2.nom = "dupont";
+		p2.prenom = "jeannot";
+		p2.adresse = "22 rue des cons";
+		
+		p1.affichage();
+		p2.affichage();
+	}
 
 }
